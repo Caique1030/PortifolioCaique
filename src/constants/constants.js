924aea381f2e7a858961a5589185f35319c9cff4
caique <kaikejrsilva55@gmail.com>
2024-07-17 19:25:40 -0300

    mudança no front

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,68 +1,68 @@
 export const projects = [
+  // {
+  //   type: 'front',
+  //   title: 'Trybe Wallet TypeScript',
+  //   description:"O projeto foi criado no intuito de itensificar os estudos de front-end utilizando React, Redux, EsLint para qualidade do código apresentado utilizando o padrão Airnb, esse projeto possui uma tela de login e após entrar no app é possível cadastrar atividades e fazer cotações das moedas em tempo real utilizando a api.",
+  //   image: '/images/ezgif.com-gif-maker (1).gif',
+  //   tags: ['React', 'Redux', 'TypeScript', 'HTML5', 'Bootstrap'],
+  //   source: 'https://github.com/rafaelromanoz/trybewallet-hooks-typescript',
+  //   visit: 'https://rafaelromanoz.github.io/trybewallet-hooks-typescript/',
+  //   id: 1
+  // },
+  // {
+  //   title: 'Trivia Game',
+  //   description: "O projeto Trivia, é um jogo de perguntas e respostas (tipo um show do milhão) onde é consumida a API do Jogo Trivia, cada pergunta tem o tempo de 30 segundos para ser respondida, é possível consultar ranking, verificar qual a resposta certa ou errada, projeto feito em grupo, onde foi possível treinar muita assertividade, empatia e cooperação.",
+  //     image: '/images/trivia.gif',
+  //     tags: ['React', 'Redux', 'CSS3','HTML5'],
+  //   source: 'https://github.com/rafaelromanoz/trivia-react-redux',
+  //   visit: 'https://rafaelromanoz.github.io/trivia-react-redux/',
+  //   id: 2
+  // }, 
+  // {
+  //   title: 'App de Receitas',
+  //   description: "O projeto é um App de Receita Web , onde são consultadas 12 endpoints em APIs, no app é possível pesquisar receitas de comidas e bebidas por nome, categoria, tipo, favoritar, marcar ingredientes, exibir detalhes, entre outras funcionalidades. Tecnologias usadas : React, React-Redux, Rect-Router, Git/Github flow, Lint entre outras tecnologias. Praticamos bastante soft skills como empatia, assertividade, comunicação, trabalho em grupo.",
+  //     image: '/images/appreceitasref.gif',
+  //     tags: ['React', 'Redux', 'CSS3', 'HTML5'],
+  //   source: 'https://github.com/rafaelromanoz/recipes-app-react',
+  //   visit: 'https://rafaelromanoz.github.io/recipes-app-react/',
+  //   id: 3
+  // },
   {
-    type: 'front',
-    title: 'Trybe Wallet TypeScript',
-    description:"O projeto foi criado no intuito de itensificar os estudos de front-end utilizando React, Redux, EsLint para qualidade do código apresentado utilizando o padrão Airnb, esse projeto possui uma tela de login e após entrar no app é possível cadastrar atividades e fazer cotações das moedas em tempo real utilizando a api.",
-    image: '/images/ezgif.com-gif-maker (1).gif',
-    tags: ['React', 'Redux', 'TypeScript', 'HTML5', 'Bootstrap'],
-    source: 'https://github.com/rafaelromanoz/trybewallet-hooks-typescript',
-    visit: 'https://rafaelromanoz.github.io/trybewallet-hooks-typescript/',
-    id: 1
-  },
-  {
-    title: 'Trivia Game',
-    description: "O projeto Trivia, é um jogo de perguntas e respostas (tipo um show do milhão) onde é consumida a API do Jogo Trivia, cada pergunta tem o tempo de 30 segundos para ser respondida, é possível consultar ranking, verificar qual a resposta certa ou errada, projeto feito em grupo, onde foi possível treinar muita assertividade, empatia e cooperação.",
-      image: '/images/trivia.gif',
-      tags: ['React', 'Redux', 'CSS3','HTML5'],
-    source: 'https://github.com/rafaelromanoz/trivia-react-redux',
-    visit: 'https://rafaelromanoz.github.io/trivia-react-redux/',
-    id: 2
-  }, 
-  {
-    title: 'App de Receitas',
-    description: "O projeto é um App de Receita Web , onde são consultadas 12 endpoints em APIs, no app é possível pesquisar receitas de comidas e bebidas por nome, categoria, tipo, favoritar, marcar ingredientes, exibir detalhes, entre outras funcionalidades. Tecnologias usadas : React, React-Redux, Rect-Router, Git/Github flow, Lint entre outras tecnologias. Praticamos bastante soft skills como empatia, assertividade, comunicação, trabalho em grupo.",
-      image: '/images/appreceitasref.gif',
-      tags: ['React', 'Redux', 'CSS3', 'HTML5'],
-    source: 'https://github.com/rafaelromanoz/recipes-app-react',
-    visit: 'https://rafaelromanoz.github.io/recipes-app-react/',
-    id: 3
-  },
-  {
-    title: 'Pixel ART',
+    title: 'Pixel Picture',
     description: "Um projeto muito divertido de se fazer, criando uma paleta de cores manipulando o DOM em JavaScript, HTML, CSS.",
       image: '/images/pixart.gif',
       tags: ['JavaScript', 'HTML', 'CSS3','DOM'],
-    source: 'https://github.com/rafaelromanoz/pixel-art-js-html',
-    visit: 'https://pixel-art-js-html.vercel.app/',
+    source: 'https://github.com/Caique1030/PixelArt',
+    visit: 'pixel-rb0p5stdq-caique-junior-da-silvas-projects.vercel.app',
     id: 4
   },
-  {
-    title: 'Star Wars Search Planet',
-    description: "O projeto Star Wars Planet foi desenvolvido utilizando uma API de dados do Star Wars, junto com React e Context API, nesse app pode se filtrar por nome e outras características dos planetas de Star Wars.",
-      image: '/images/sw.gif',
-      tags: ['React', 'Context API', 'CSS3','HTML5', 'Material UI'],
-    source: 'https://github.com/rafaelromanoz/starwars-planet-react',
-    visit: 'https://starwars-planet-react.vercel.app/',
-    id: 5
-  },
-  {
-    title: 'Movie Card Stateful',
-    description: "Projeto feito em React, CSS, para aperfeiçoar os aprendizados sobre componentes controlados, usando Cards.",
-      image: '/images/mcs.gif',
-      tags: ['React', 'CSS3','HTML5'],
-    source: 'https://github.com/rafaelromanoz/movie-cards-libraryStateful-react',
-    visit: 'https://movie-cards-library-stateful-react.vercel.app/',
-    id: 6
-  },
-  {
-    title: 'Frontend Online Store',
-    description: "Projeto feito em grupo, para reforçar conceitos de React, assincronismo, metodologias ágeis como Kanban, Scrum, para organização do trabalho foi utilizado a ferramenta Trello, para desenvolvimento do APP foi utilizado a API gratuita do Mercado Livre, podendo pesquisar, adicionar itens no carrinho realizando a soma de cada item.",
-      image: '/images/sc.gif',
-      tags: ['React', 'CSS3','HTML5', 'Kanban', 'Scrum'],
-    source: 'https://github.com/rafaelromanoz/online-frontend-store-react',
-    visit: 'https://online-frontend-store-react-7nw7rk82g-rafaelromanoz.vercel.app/',
-    id: 7
-  }
+  // {
+  //   title: 'Star Wars Search Planet',
+  //   description: "O projeto Star Wars Planet foi desenvolvido utilizando uma API de dados do Star Wars, junto com React e Context API, nesse app pode se filtrar por nome e outras características dos planetas de Star Wars.",
+  //     image: '/images/sw.gif',
+  //     tags: ['React', 'Context API', 'CSS3','HTML5', 'Material UI'],
+  //   source: 'https://github.com/rafaelromanoz/starwars-planet-react',
+  //   visit: 'https://starwars-planet-react.vercel.app/',
+  //   id: 5
+  // },
+  // {
+  //   title: 'Movie Card Stateful',
+  //   description: "Projeto feito em React, CSS, para aperfeiçoar os aprendizados sobre componentes controlados, usando Cards.",
+  //     image: '/images/mcs.gif',
+  //     tags: ['React', 'CSS3','HTML5'],
+  //   source: 'https://github.com/rafaelromanoz/movie-cards-libraryStateful-react',
+  //   visit: 'https://movie-cards-library-stateful-react.vercel.app/',
+  //   id: 6
+  // },
+  // {
+  //   title: 'Frontend Online Store',
+  //   description: "Projeto feito em grupo, para reforçar conceitos de React, assincronismo, metodologias ágeis como Kanban, Scrum, para organização do trabalho foi utilizado a ferramenta Trello, para desenvolvimento do APP foi utilizado a API gratuita do Mercado Livre, podendo pesquisar, adicionar itens no carrinho realizando a soma de cada item.",
+  //     image: '/images/sc.gif',
+  //     tags: ['React', 'CSS3','HTML5', 'Kanban', 'Scrum'],
+  //   source: 'https://github.com/rafaelromanoz/online-frontend-store-react',
+  //   visit: 'https://online-frontend-store-react-7nw7rk82g-rafaelromanoz.vercel.app/',
+  //   id: 7
+  // }
 ];
 
 export const TimeLineData = [
